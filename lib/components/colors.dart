95912e8402f1549266eb292e923a712858175081
Pratiksharmaps <122214913+Pratiksharmaps@users.noreply.github.com>
2024-06-10 08:30:04 +0530

    colors

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -3,5 +3,5 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color.fromARGB(255, 1, 93, 168);
-const Color secondaryColor = Color.fromARGB(213, 222, 34, 103);
+const Color secondaryColor = Color.fromARGB(212, 222, 34, 62);
 
